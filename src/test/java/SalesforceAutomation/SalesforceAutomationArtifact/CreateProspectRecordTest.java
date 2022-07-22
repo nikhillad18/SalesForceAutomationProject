bce1b2f691ca775b3c39e35bbd59fdeaa7ae1e67
Lad <niklad@deloitte.com>
2022-07-22 19:22:14 +0530

    Added in constructor in CreateProspectTest

--- src/test/java/SalesforceAutomation/SalesforceAutomationArtifact/CreateProspectRecordTest.java
+++ src/test/java/SalesforceAutomation/SalesforceAutomationArtifact/CreateProspectRecordTest.java
@@ -1,5 +1,12 @@
 package SalesforceAutomation.SalesforceAutomationArtifact;
 
+import org.openqa.selenium.WebDriver;
+
 public class CreateProspectRecordTest {
+	public WebDriver driver;
+
+	public CreateProspectRecordTest() {
+		
+	}
 
 }
